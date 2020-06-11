426507c2636cbc0de36298fcca8654d352286dee
Dalton <C.Dalton@mitsui.com>
2020-06-11 08:47:00 -0500

    changed prop to setTotal for cohesion

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
 
-const Counter = (props) => {
+const Counter = (setTotal) => {
     const [state,setState] = useState(0);
     
     return(
         <React.Fragment>
           <h1>{state}</h1>
           <button onClick = {() =>{
-              props.updateFunction(i => (i+1)) 
+              setTotal.updateFunction(i => (i+1)) 
               setState(state + 1)
               }}>Button</button>
         </React.Fragment>
